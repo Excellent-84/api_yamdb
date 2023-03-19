5a0b04e19864f03417f8836d1db741be3e726ec1
BelovRV89 <belov.ruslan@list.ru>
2023-03-19 13:54:33 +0300

    последние исправления,12 ошибок осталось

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,11 +1,11 @@
 from rest_framework import permissions
 
 
-class IsAdminOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
+class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
                 request.method in permissions.SAFE_METHODS
-                or (request.user.is_admin and request.user.is_authenticated)
+                or (request.user.is_authenticated and request.user.is_admin)
         )
 
 
